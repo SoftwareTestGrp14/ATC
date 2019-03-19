9bb1357eb4acbca95a453056701fb441ac583cc9
Jonas <au544262@post.au.dk>
2019-03-19 15:21:07 +0100

    removed parameters from faketracks

--- UnitTest/TestSeparationCondition.cs
+++ UnitTest/TestSeparationCondition.cs
@@ -13,8 +13,8 @@ namespace UnitTests3 {
         [SetUp]
         public void Setup()
         {
-            _fakeTrack1 = Substitute.For<ITrack>("ATR423", 39045, 12932, 140000, 20151006213456789);
-            _fakeTrack2 = Substitute.For<ITrack>("BTU423", 30000, 15000, 100000, 20151006213456789);
+            _fakeTrack1 = Substitute.For<ITrack>();
+            _fakeTrack2 = Substitute.For<ITrack>();
 
 
             equalsSeparation = new SeparationCondition(_fakeTrack1, _fakeTrack2);
