514c9af6543cfd1f53a6bc3a98adac8247f8f01f
Jonas <au544262@post.au.dk>
2019-03-20 10:24:31 +0100

    Fixed error in track that checked the parameters

--- ATC/AirSpaceTracker.cs
+++ ATC/AirSpaceTracker.cs
@@ -8,21 +8,25 @@ namespace ATC
     {
         public bool IsInAirSpace(IAirSpace airSpace, ITrack track)
         {
+            Console.WriteLine("TEST1TestTest");
             if (track._alt < airSpace.MinAltitude || track._alt > airSpace.MaxAltitude)
             { 
+                Console.WriteLine("TEST1");
                 return false;
             }
 
             if (track._xCord < airSpace.XStartPoint || track._xCord > airSpace.GetXEndPoint())
             {
+                Console.WriteLine("TEST2");
                 return false;
             }
 
             if (track._yCord < airSpace.YStartPoint || track._yCord > airSpace.GetYEndPoint())
             {
+                Console.WriteLine("TEST3");
                 return false;
             }
-
+            Console.WriteLine("TEST4");
             return true;
         }
     }
