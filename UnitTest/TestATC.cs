3b65139ee4afae91b5ccebeabf77d87304563b02
Thomas <thomas.moeller.1996@gmail.com>
2019-03-21 12:07:57 +0100

    Added ATC test

--- UnitTest/TestATC.cs
+++ UnitTest/TestATC.cs
@@ -6,26 +6,54 @@ using System.Threading.Tasks;
 using ATC;
 using NSubstitute;
 using NUnit.Framework;
+using TransponderReceiver;
 
 namespace UnitTest
 {
-    /*class TestATC
+    [TestFixture]
+    class TestATC
     {
-        private ATM _fakeATM;
         private ATC.ATC _uut;
+        private ATM atm;
+        private ITransponderReceiver _fakeTransponderReceiver;
+        private IPlaneTracker _fakePlaneTracker;
 
         [SetUp]
-        public void setup()
+        public void Setup()
         {
-            _fakeATM = Substitute.For<ATM>();
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            _fakePlaneTracker = Substitute.For<IPlaneTracker>();
+            atm = new ATM(_fakePlaneTracker, _fakeTransponderReceiver);
+            _uut = new ATC.ATC(atm);
         }
 
         [Test]
         public void Constructor_test()
         {
-            _uut = new ATC.ATC(_fakeATM);
-
-            Assert.That(_uut, Is.Not.EqualTo(null));
+            Assert.That(_uut.ATM, Is.EqualTo(atm));
         }
-    }*/
+
+        [Test]
+        public void ATMset_SetToNewATM_ATMgetEqNewATM()
+        {
+            ITransponderReceiver _newFakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            IPlaneTracker _newFakePlaneTracker = Substitute.For<IPlaneTracker>();
+            ATM newAtm = new ATM(_newFakePlaneTracker, _newFakeTransponderReceiver);
+            _uut.ATM = newAtm;
+
+            Assert.That(_uut.ATM, Is.EqualTo(newAtm));
+        }
+
+        [Test]
+        public void ATMset_SetToNewATM_ATMgetNotEqOldATM()
+        {
+            ITransponderReceiver _newFakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            IPlaneTracker _newFakePlaneTracker = Substitute.For<IPlaneTracker>();
+            ATM newAtm = new ATM(_newFakePlaneTracker, _newFakeTransponderReceiver);
+            _uut.ATM = newAtm;
+
+            Assert.That(_uut.ATM, Is.Not.EqualTo(atm));
+        }
+    }
 }
+
