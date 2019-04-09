fa5e8c2311952831d5a78b8c5b07971fb2a549b0
Jonas <au544262@post.au.dk>
2019-04-09 14:36:03 +0200

    Added tracksmanager

---
+++ ATC/TracksManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATC
+{
+    public class TracksManager:ITracksManager
+    {
+        public TracksManager()
+        {
+
+        }
+
+        public List<ITrack> RemoveAt(List<ITrack> tracks, int index)
+        {
+            tracks.RemoveAt(index);
+
+            return tracks;
+        }
+
+        public List<ITrack> AddTrack(List<ITrack> tracks, ITrack track)
+        {
+            tracks.Add(track);
+
+            return tracks;
+
+        }
+
+
+    }
+}
