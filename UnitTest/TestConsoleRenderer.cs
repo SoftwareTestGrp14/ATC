24f4b84254b5e7ea33b8cbd17fc27059e4370ec4
Jonas <au544262@post.au.dk>
2019-04-10 11:13:49 +0200

    ConsoleRendererTestet

--- UnitTest/TestConsoleRenderer.cs
+++ UnitTest/TestConsoleRenderer.cs
@@ -31,11 +31,33 @@ namespace UnitTest
         public void Test_RenderCalledWithTrackList_WriteCalled()
         {
             List<ITrack> tracks = new List<ITrack>();
+            Track track1 = new Track("ABC123", Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<double>(), Arg.Any<double>(), DateTime.Now);
+            Track track2 = new Track("DEF456", Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<double>(), Arg.Any<double>(), DateTime.Now);
+
+            tracks.Add(track1);
+            tracks.Add(track2);
 
             _uut.Render(tracks);
 
 
             _fakeConsoleLogger.Received().Write(Arg.Any<string>());
+
+        }
+
+        [Test]
+        public void Test_RenderCalledWithTrackList_WriteCalledCountPlus1Times()
+        {
+            List<ITrack> tracks = new List<ITrack>();
+            Track track1 = new Track("ABC123", Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<double>(), Arg.Any<double>(), DateTime.Now);
+            Track track2 = new Track("DEF456", Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<double>(), Arg.Any<double>(), DateTime.Now);
+
+            tracks.Add(track1);
+            tracks.Add(track2);
+
+            _uut.Render(tracks);
+
+
+            _fakeConsoleLogger.Received(tracks.Count+1).Write(Arg.Any<string>());
 
         }
 
@@ -49,6 +71,28 @@ namespace UnitTest
 
 
             _fakeConsoleLogger.Received().Write(Arg.Any<string>());
+
+        }
+
+
+        [Test]
+        public void Test_RenderCalledWithSeparationConditionList_WriteCalledCountPlus1Times()
+        {
+            List<ISeparationCondition> separations = new List<ISeparationCondition>();
+
+            Track track1 = new Track("ABC123", Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<double>(), Arg.Any<double>(), DateTime.Now);
+            Track track2 = new Track("DEF456", Arg.Any<int>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<double>(), Arg.Any<double>(), DateTime.Now);
+
+            SeparationCondition sep1 = new SeparationCondition(track1, track2);
+            SeparationCondition sep2 = new SeparationCondition(track1, track2);
+
+            separations.Add(sep1);
+            separations.Add(sep2);
+
+            _uut.Render(separations);
+
+
+            _fakeConsoleLogger.Received(separations.Count).Write(Arg.Any<string>());
 
         }
     }
