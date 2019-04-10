03e23667d8b041d80d16d9909036f7b800b6f4f7
Oskarthansen <oskarthansen@outlook.dk>
2019-04-10 09:42:58 +0200

    Changes

---
+++ ATC/ConsoleRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATC
+{
+    public class ConsoleRenderer : IConsoleRenderer
+    {
+        private ILogger _logger;
+        public ConsoleRenderer(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Render(List<ITrack> tracks)
+        {
+            foreach (var track in tracks)
+            {
+                string formattedTrack =
+                    $"Tag: {track._tag} - (X,Y): ({track._xCord}, {track._yCord}) - Alt: {track._alt} - Vel: {track._velocity}M/s - Course: {track._course}";
+                _logger.Write(formattedTrack);
+            }
+        }
+
+        public void Render(List<ISeparationCondition> separations)
+        {
+            foreach (var separation in separations)
+            {
+                string formattedSeparation =
+                    $"Separation between: {separation._track1._tag} and {separation._track2._tag}";
+                _logger.Write(formattedSeparation);
+            }
+        }
+    }
+}
---
+++ ATC/FileRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATC
+{
+    public class FileRenderer : IFileRenderer
+    {
+        private ILogger _logger;
+        public FileRenderer(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Render(ISeparationCondition separation)
+        {
+            var formattedSeparation =
+                $"Separation condition detected at {separation._track1._tag} and {separation._track2._tag} at timestamp: {separation.Timestamp}";
+            _logger.Write(formattedSeparation);
+        }
+    }
+}
