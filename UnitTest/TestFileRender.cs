eec552828d9fb4328f4ce70f67bc476acaf56b4a
Thomas <thomas.moeller.1996@gmail.com>
2019-04-10 11:35:01 +0200

    Added another test for FileRender

--- UnitTest/TestFileRender.cs
+++ UnitTest/TestFileRender.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using ATC;
 using NSubstitute;
+using NSubstitute.Core.Arguments;
 using NUnit.Framework;
 
 namespace UnitTest
@@ -31,6 +33,16 @@ namespace UnitTest
             logger.Received(1).Write(Arg.Any<string>());
         }
 
+        [Test]
+        public void RenderTest_RenderCalled_WriteInLoggerCalled_WithRightParameter()
+        {
+            ISeparationCondition newSeparationCondition = new SeparationCondition(
+                new Track("ATR423", 0,0, 500, 1,1,DateTime.Now), 
+                new Track("ABC123", 0, 0, 500, 1, 1, DateTime.Now));
 
+            _uut.Render(newSeparationCondition);
+
+            logger.Received(1).Write(Arg.Is<string>(x=>x.Contains("ATR423") && x.Contains("ABC123")));
+        }
     }
 }
