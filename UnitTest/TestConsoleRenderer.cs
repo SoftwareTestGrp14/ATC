e7f0cf0ee46015a3b7047112b13b1007a8b4d8c9
Jonas <au544262@post.au.dk>
2019-04-10 10:57:53 +0200

    Testing Console Renderer

---
+++ UnitTest/TestConsoleRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATC;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace UnitTest
+{
+    class TestConsoleRenderer
+    {
+
+        private ConsoleRenderer _uut;
+        private ILogger _fakeConsoleLogger;
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+            _fakeConsoleLogger = Substitute.For<ILogger>();
+
+            _uut = new ConsoleRenderer(_fakeConsoleLogger);
+        }
+
+
+        [Test]
+        public void Test_RenderCalledWithTrackList_WriteCalled()
+        {
+            List<ITrack> tracks = new List<ITrack>();
+
+            _uut.Render(tracks);
+
+
+            _fakeConsoleLogger.Received().Write(Arg.Any<string>());
+
+        }
+
+
+        [Test]
+        public void Test_RenderCalledWithSeparationConditionList_WriteCalled()
+        {
+            List<ISeparationCondition> separations = new List<ISeparationCondition>();
+
+            _uut.Render(separations);
+
+
+            _fakeConsoleLogger.Received().Write(Arg.Any<string>());
+
+        }
+    }
+}
